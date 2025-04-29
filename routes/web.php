57ea10a1db018efc068616750d58064ac8f98c1b
Simon-kercode <149500799+Simon-kercode@users.noreply.github.com>
2025-04-29 12:54:56 +0200

    breeze & project controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\Admin\ProjectController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
+    Route::resource('projects', ProjectController::class);
 });
